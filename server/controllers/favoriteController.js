d99fa7c3cf756ecab11bbebd7ac65d06fa3586f5
João Pedro Guttierrez <jpgutti08@hotmail.com>
2019-05-31 02:39:23 -0300

    commited unfinished logic

--- server/controllers/favoriteController.js
+++ server/controllers/favoriteController.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const User = require("../models/User");
+// const express = require('express');
+// const User = require("../models/User");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/favorite', async (req, res) => {
+// router.post('/favorite', async (req, res) => {
 
-    const { _id, favorite } = req.body;
+//     const { _id, favorite } = req.body;
 
-    try{
-        const user = await User.findById({ _id });
+//     try{
+//         const user = await User.findById({ _id });
 
-        if(!user){
-            return res.status(400).send({ error: "User not found" });
-        }
+//         if(!user){
+//             return res.status(400).send({ error: "User not found" });
+//         }
 
-        user.favoriteList.push(favorite);
-
-
-        res.send({ user })
+//         user.favoriteList.push(favorite);
 
 
-    } catch(err){
-        console.log(err)
-        return res.status(400).send({error: err})
-    }
+//         res.send({ user })
 
-}) 
+
+//     } catch(err){
+//         console.log(err)
+//         return res.status(400).send({error: err})
+//     }
+
+// }) 
 
 // module.exports = app => app.use('/user', router)
